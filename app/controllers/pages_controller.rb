2b932880ede332801407a5d0528931c63ea54dbe
shenxin <wb_shenxin@meituan.com>
2022-08-27 13:25:33 +0800

    feat: first route and html link

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    # render json: {
+    #   message: "Welcome to the home page!"
+    # }
+  end
+
+  def about
+    # render json: {
+    #   message: "Welcome to the about page!"
+    # }
+  end
+end
